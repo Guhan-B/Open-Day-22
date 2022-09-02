755c97630e988e38c471d2cea9a3ea0db191a089
Guhan.B <bkguhan2001@gmail.com>
2022-09-02 13:57:27 +0530

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: true,
+        autoplay: true,
+        speed: 1000,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+});
+
+
+const navbar = document.querySelector(".navigation_wrapper");
+const links = document.getElementById("links");
+const toggle = document.querySelector(".hamburger");
+
+document.addEventListener('scroll', function (e) {
+    if (window.scrollY > 10) {
+        navbar.style.boxShadow = "rgba(0, 0, 0, 0.2) 0px 0px 5px 0px";
+        console.log(navbar.style.boxShadow)
+    } else {
+        navbar.style.boxShadow = ""
+    }
+});
+
+toggle.addEventListener("click",() => {
+    toggle.classList.toggle("is-active")
+    links.classList.toggle("visible");
+})
